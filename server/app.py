b391ddcb896eea218416417475a364f78115c1b4
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-30 16:01:12 -0600

    hm

--- server/app.py
+++ server/app.py
@@ -4,11 +4,33 @@ from flask import request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
-from config import app, db, api
+from config import app, db, api, ma
 from models import User, Recipe
 
+class SignupSchema(ma.SQLAlchemySchema):
+
+    class Meta:
+        model = User
+
+    title = ma.auto_field()
+    published_at = ma.auto_field()
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "newsletterbyid",
+                values=dict(id="<id>")),
+            "collection": ma.URLFor("newsletters"),
+        }
+    )
+
+newsletter_schema = NewsletterSchema()
+newsletters_schema = NewsletterSchema(many=True)
+
 class Signup(Resource):
-    pass
+
+    def post(self):
+
 
 class CheckSession(Resource):
     pass
